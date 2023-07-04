37144a09247c1dc5c789e35229ff9e1b03ff203b
Rikelmelopes <rikelme.lopes777@gmail.com>
2023-07-04 18:49:16 -0300

    Ex 78

---
+++ Listas_em_Tuplas/ex.py
@@ -0,0 +1,9 @@
+num = [2, 5, 9, 1]
+num [2] = 3 #Troca o 9 pelo 3
+num.append(7) #Adiciona no final da lista
+num.sort() #Coloca em ordem
+num.sort(reverse=True) #Coloca em ordem reversa
+num.insert(2, 0) #Insere um numero
+num.pop(2) #Apaga item da lista
+num.remove(2) #Remove item da lista
+print(num)
---
+++ Listas_em_Tuplas/ex78.py
@@ -0,0 +1,22 @@
+'''
+Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor 
+valor digitado e as suas respectivas posições na lista
+'''
+
+valores = []  # lista vazia para armazenar os valores
+
+for i in range(5):
+    valor = float(input(f"Digite o {i+1}º valor: "))
+    valores.append(valor)
+
+# Encontrar o maior valor e sua posição
+maior_valor = max(valores)
+posicao_maior = valores.index(maior_valor)
+
+# Encontrar o menor valor e sua posição
+menor_valor = min(valores)
+posicao_menor = valores.index(menor_valor)
+
+# Mostrar os resultados
+print(f"O maior valor digitado foi {maior_valor} na posição {posicao_maior+1}")
+print(f"O menor valor digitado foi {menor_valor} na posição {posicao_menor+1}")
